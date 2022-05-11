02377a0f0969e40e9e8fae0d16776572814ae7b1
serdar sayın <serdarsayin61@gmail.com>
2022-05-11 09:14:22 +0300

    text learn

---
+++ lib/101/text_learn_view.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class TextLearnView extends StatelessWidget {
+  const TextLearnView({Key? key}) : super(key: key);
+  final String name = "serdar";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            ('Welcome $name ${name.length}'),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.headline5?.copyWith(
+                  color: ProjectColors.welcomeColor,
+                  decoration: TextDecoration.underline,
+                ),
+          ),
+          Text('Deneme',
+              style: Theme.of(context).textTheme.headline5?.copyWith(
+                    color: ProjectColors.welcomeColor,
+                  ))
+        ],
+      )),
+    );
+  }
+}
+
+class ProjectStyles {
+  static TextStyle welcomeStyle = const TextStyle(
+      wordSpacing: 2,
+      decoration: TextDecoration.underline,
+      fontStyle: FontStyle.italic,
+      color: Colors.red,
+      letterSpacing: 2,
+      fontSize: 16,
+      fontWeight: FontWeight.w600);
+}
+
+class ProjectColors {
+  static Color welcomeColor = Colors.red;
+}
